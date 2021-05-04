532311feecae4f8d392abdd3fb25b6a8d2a019ab
jason <kwon_jason@yahoo.com>
2021-05-04 08:58:27 +0300

    1155 tracker

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+require('./models/account')
+require('./models/bundle')
+require('./models/event')
+require('./models/tradehistory')
+require('./models/collection')
+require('./models/abi')
+require('./models/listing')
+require('./models/notification')
+require('./models/bid')
+require('./models/highestblock')
+require('./models/offer')
+require('./models/category')
+require('./models/erc1155contract')
+require('./models/erc1155token')
+
+const trackNewERC1155 = require('./services/erc1155tracker')
+
+const uri = process.env.DB_URL
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', async () => {
+  console.log('nifty server has been connected to the db server')
+  trackNewERC1155()
+})
---
+++ services/erc1155tracker.js
@@ -0,0 +1,130 @@
+require('dotenv').config()
+const ethers = require('ethers')
+const mongoose = require('mongoose')
+const ERC1155CONTRACT = mongoose.model('ERC1155CONTRACT')
+const Category = mongoose.model('Category')
+const ERC1155TOKEN = mongoose.model('ERC1155TOKEN')
+
+const SimplifiedERC1155ABI = require('../constants/simplified1155abi')
+const isValidERC1155 = require('../utils/ERC1155Validator')
+
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.MAINNET_RPC,
+  250,
+)
+
+let remainder = process.env.REMAINDER
+
+const handleSingleTransfer = async (
+  operator,
+  from,
+  to,
+  id,
+  value,
+  contract,
+) => {
+  let tk = await ERC1155TOKEN.find({ contractAddress: operator, tokenID: id })
+  if (tk) {
+    //add value to the receiver
+    let ownerMap = tk.owner
+    if (ownerMap) {
+      try {
+        let senderValue = ownerMap.get(from)
+        if (senderValue >= value) {
+          senderValue = senderValue - value
+          ownerMap.set(from, senderValue)
+        }
+        //deduct value from sender
+        let receiverValue = ownerMap.get(to)
+        receiverValue = receiverValue + value
+        ownerMap.set(to, receiverValue)
+        tk.owner = ownerMap
+        await tk.save()
+      } catch (error) {}
+    } else {
+      // ???
+    }
+  } else {
+    // this is new mint
+    // add new token
+    let newTk = new ERC1155TOKEN()
+    let ownerMap = new Map()
+    ownerMap.set(to, value)
+    newTk.owner = ownerMap
+    newTk.contractAddress = operator
+    newTk.tokenID = id
+    let uri = await contract.uri(id)
+    newTk.tokenURI = uri
+    await newTk.save()
+  }
+}
+
+const erc1155EventHandler = async (address) => {
+  let abi = SimplifiedERC1155ABI
+  let contract = new ethers.Contract(address, abi, provider)
+  contract.on('TransferSingle', async (operator, from, to, id, value) => {
+    await handleSingleTransfer(operator, from, to, id, value, contract)
+  })
+  contract.on('TransferBatch', async (operator, from, to, ids, values) => {
+    let promises = ids.map(async (_, index) => {
+      await handleSingleTransfer(
+        operator,
+        from,
+        to,
+        ids[index],
+        values[index],
+        contract,
+      )
+    })
+    Promise.all(promises)
+  })
+  contract.on('URI', async (value, id) => {})
+}
+
+const trackERC1155 = async (tnx) => {
+  let contractAddress = tnx.creates
+  let name = tnx.tokenName
+  let symbol = tnx.tokenSymbol
+  let erc1155 = await ERC1155CONTRACT.findOne({ address: contractAddress })
+  if (!erc1155) {
+    let minter = new ERC1155CONTRACT()
+    minter.address = contractAddress
+    minter.name = name
+    minter.symbol = symbol
+    await minter.save()
+    let category = new Category()
+    category.minterAddress = contract.address
+    category.type = 1155
+    await category.save()
+    await erc1155EventHandler(contractAddress)
+  }
+  //   save new contract and start tracking
+}
+
+const trackNewERC1155 = async () => {
+  provider.on('block', async (blockNumber) => {
+    let _remainder = blockNumber % 6
+    if (_remainder == remainder) {
+      console.log(blockNumber)
+      let block = await provider.getBlockWithTransactions(blockNumber)
+      let tnxs = block.transactions
+      if (tnxs.length > 0) {
+        let promises = tnxs.map(async (tnx) => {
+          if (tnx.creates != null) {
+            console.log('1155 new tnx')
+            let contractAddress = tnx.creates
+            let isERC1155 = await isValidERC1155(contractAddress)
+            if (isERC1155) {
+              console.log('1155 new sc')
+              await trackERC1155(tnx)
+            }
+          }
+        })
+
+        Promise.all(promises)
+      }
+    }
+  })
+}
+
+module.exports = trackNewERC1155
