e3440646f3fd8f308f07af2e1dacbd3e34f31fc9
jason <kwon_jason@yahoo.com>
2021-05-18 18:57:34 +0300

    1155 transfer tracker

--- services/erc1155tracker.js
+++ services/erc1155tracker.js
@@ -10,66 +10,19 @@ const provider = new ethers.providers.JsonRpcProvider(
   process.env.MAINNET_RPC,
   parseInt(process.env.MAINNET_CHAINID),
 )
+
+const toLowerCase = (val) => {
+  if (val) return val.toLowerCase()
+  else return val
+}
+
+const validatorAddress = '0x0000000000000000000000000000000000000000'
 // store trackedAddresses
 const trackedAddresses = []
 const trackedSCs = []
 
-const handleSingleTransfer = async (
-  operator,
-  from,
-  to,
-  id,
-  value,
-  contract,
-) => {
-  try {
-    let tk = await ERC1155TOKEN.find({ contractAddress: operator, tokenID: id })
-    if (tk) {
-      //add value to the receiver
-      let ownerMap = tk.owner
-      if (ownerMap) {
-        try {
-          let senderValue = ownerMap.get(from)
-          if (senderValue >= value) {
-            senderValue = senderValue - value
-            if (senderValue == value) {
-              ownerMap.delete(from)
-            } else {
-              ownerMap.set(from, senderValue)
-            }
-          }
-          //deduct value from sender
-          let receiverValue = ownerMap.get(to)
-          receiverValue = receiverValue + value
-          ownerMap.set(to, receiverValue)
-          tk.owner = ownerMap
-          await tk.save()
-        } catch (error) {}
-      } else {
-        // ???
-      }
-    } else {
-      // this is new mint
-      // add new token
-      let newTk = new ERC1155TOKEN()
-      let ownerMap = new Map()
-      ownerMap.set(to, value)
-      newTk.owner = ownerMap
-      newTk.contractAddress = operator
-      newTk.tokenID = id
-      let uri = await contract.uri(id)
-      newTk.tokenURI = uri
-      await newTk.save()
-    }
-  } catch (error) {
-    console.log('overall handle single transfer function')
-    console.log(error)
-  }
-}
 const trackNewERC1155 = async () => {
   const func = async () => {
-    console.log('tracked addresses are ')
-    console.log(trackedAddresses)
     try {
       let untrackedSCs = await ERC1155CONTRACT.find({
         address: { $nin: trackedAddresses },
@@ -86,20 +39,120 @@ const trackNewERC1155 = async () => {
           contract.on(
             'TransferSingle',
             async (operator, from, to, id, value) => {
-              await handleSingleTransfer(
-                operator,
-                from,
-                to,
-                id,
-                value,
-                contract,
-              )
+              operator = toLowerCase(operator)
+              from = toLowerCase(from)
+              to = toLowerCase(to)
+              id = parseFloat(id.toString())
+              value = parseFloat(value.toString())
+              try {
+                if (from == validatorAddress) {
+                  // this is a new mint
+                  let newTk = new ERC1155TOKEN()
+                  let ownerMap = new Map()
+                  ownerMap.set(to, value)
+                  newTk.owner = ownerMap
+                  newTk.contractAddress = address
+                  newTk.tokenID = id
+                  newTk.supply = value
+                  newTk.tokenURI = 'https://'
+                  await newTk.save()
+                } else {
+                  let tk = await ERC1155TOKEN.findOne({
+                    contractAddress: operator,
+                    tokenID: id,
+                  })
+                  if (tk) {
+                    //add value to the receiver
+                    let ownerMap = tk.owner
+                    if (ownerMap) {
+                      try {
+                        let senderValue = ownerMap.get(from)
+                        let remainingBalance = parseFloat(senderValue) - value
+                        if (remainingBalance == 0) {
+                          ownerMap.delete(from)
+                        } else {
+                          ownerMap.set(from, remainingBalance)
+                        }
+                        //deduct value from sender
+                        let receiverValue = ownerMap.get(to)
+                        receiverValue = receiverValue + value
+                        ownerMap.set(to, receiverValue)
+                        tk.owner = ownerMap
+                        await tk.save()
+                      } catch (error) {
+                        console.log('in save while there is a tk')
+                        console.log(error)
+                      }
+                    } else {
+                      console.log('else')
+                    }
+                  }
+                }
+              } catch (error) {
+                console.log('overall handle single transfer function')
+                console.log(error)
+              }
             },
           )
           contract.on(
             'TransferBatch',
             async (operator, from, to, ids, values) => {
               let promises = ids.map(async (_, index) => {
+                operator = toLowerCase(operator)
+                from = toLowerCase(from)
+                to = toLowerCase(to)
+                let id = ids[index]
+                id = parseFloat(id.toString())
+                let value = values[index]
+                value = parseFloat(value.toString())
+                try {
+                  if (from == validatorAddress) {
+                    // this is a new mint
+                    let newTk = new ERC1155TOKEN()
+                    let ownerMap = new Map()
+                    ownerMap.set(to, value)
+                    newTk.owner = ownerMap
+                    newTk.contractAddress = address
+                    newTk.tokenID = id
+                    newTk.supply = value
+                    newTk.tokenURI = 'https://'
+                    await newTk.save()
+                  } else {
+                    let tk = await ERC1155TOKEN.findOne({
+                      contractAddress: operator,
+                      tokenID: id,
+                    })
+                    if (tk) {
+                      //add value to the receiver
+                      let ownerMap = tk.owner
+                      if (ownerMap) {
+                        try {
+                          let senderValue = ownerMap.get(from)
+                          let remainingBalance = parseFloat(senderValue) - value
+                          if (remainingBalance == 0) {
+                            ownerMap.delete(from)
+                          } else {
+                            ownerMap.set(from, remainingBalance)
+                          }
+                          //deduct value from sender
+                          let receiverValue = ownerMap.get(to)
+                          receiverValue = receiverValue + value
+                          ownerMap.set(to, receiverValue)
+                          tk.owner = ownerMap
+                          await tk.save()
+                        } catch (error) {
+                          console.log('in save while there is a tk')
+                          console.log(error)
+                        }
+                      } else {
+                        console.log('else')
+                      }
+                    }
+                  }
+                } catch (error) {
+                  console.log('overall handle single transfer function')
+                  console.log(error)
+                }
                 await handleSingleTransfer(
                   operator,
                   from,
@@ -112,13 +165,20 @@ const trackNewERC1155 = async () => {
               Promise.all(promises)
             },
           )
-          contract.on('URI', async (value, id) => {})
+          contract.on('URI', async (value, id) => {
+            setTimeout(async () => {
+              id = parseFloat(id.toString())
+              let tk = await ERC1155TOKEN.findOne({ tokenID: id })
+              tk.tokenURI = value
+              await tk.save()
+            }, 1000)
+          })
         })
         await Promise.all(promise)
       }
       setTimeout(async () => {
         await func()
-      }, 1000 * 1)
+      }, 1000 * 10)
     } catch (error) {
       console.log('error in tracking new 1155')
       console.log(error)
